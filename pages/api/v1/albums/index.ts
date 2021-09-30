c30edba1d922ed1691304f904827ac8b44fad26b
Steven Straatemans <steven@straatemans.nl>
2021-09-30 20:50:44 +0200

    create album REST

--- pages/api/v1/albums/index.ts
+++ pages/api/v1/albums/index.ts
@@ -9,30 +9,38 @@ import {
 } from '@server/limitResults';
 import { getAll } from '@server/data/getAll';
 import { Album } from '@ts/album';
+import { createNode } from '@server/data/createNode';
 
-const handler = baseHandler().get(async (req, res) => {
-  const { limit, offset, q } = req.query as { limit: string; offset: string; q: string };
+const handler = baseHandler()
+  .get(async (req, res) => {
+    const { limit, offset, q } = req.query as { limit: string; offset: string; q: string };
 
-  const albums = await getAll<Album>('albums');
-  const limitedResults = limitResults<Album>(albums, limit, offset, q);
-  const enrichedResults = await enrichAlbums(limitedResults);
+    const albums = await getAll<Album>('albums');
+    const limitedResults = limitResults<Album>(albums, limit, offset, q);
+    const enrichedResults = await enrichAlbums(limitedResults);
 
-  const cleanedLimit = cleanLimit(limit);
-  const cleanedOffset = cleanOffset(offset);
-  const albumCount = albums.length;
+    const cleanedLimit = cleanLimit(limit);
+    const cleanedOffset = cleanOffset(offset);
+    const albumCount = albums.length;
 
-  res.json({
-    count: albumCount,
-    next:
-      nextOffset(cleanedLimit, cleanedOffset, albumCount) > albumCount
-        ? `${process.env.APIURL}/v1/albums?limit=${cleanedLimit}&offset=${cleanedOffset}`
-        : null,
-    previous:
-      previousOffset(cleanedLimit, cleanedOffset) < 0
-        ? `${process.env.APIURL}/v1/albums?limit=${cleanedLimit}&offset=${cleanedOffset}`
-        : null,
-    results: enrichedResults,
+    res.json({
+      count: albumCount,
+      next:
+        nextOffset(cleanedLimit, cleanedOffset, albumCount) > albumCount
+          ? `${process.env.APIURL}/v1/albums?limit=${cleanedLimit}&offset=${cleanedOffset}`
+          : null,
+      previous:
+        previousOffset(cleanedLimit, cleanedOffset) < 0
+          ? `${process.env.APIURL}/v1/albums?limit=${cleanedLimit}&offset=${cleanedOffset}`
+          : null,
+      results: enrichedResults,
+    });
+  })
+  .post(async (req, res) => {
+    const { body } = req;
+
+    const data = await createNode<Album>('albums', body);
+    return res.status(201).json(data);
   });
-});
 
 export default handler;
