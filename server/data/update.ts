8939614935efd84129c80457df1ac1118ca421db
Steven Straatemans <steven@straatemans.nl>
2022-01-12 20:28:45 +0100

    update query with and without id

--- server/data/update.ts
+++ server/data/update.ts
@@ -1,6 +1,6 @@
 import { clearCache } from '@server/cache';
 import { getStore } from '.';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, addDoc, collection } from 'firebase/firestore';
 
 const addDefaultUpdateData = <T>(data: T): T => {
   return { ...data, lastUpdateDate: new Date() };
@@ -19,4 +19,21 @@ export const updateById = async <T extends { id: string }>(label: string, input:
   clearCache(label);
 
   return data as unknown as T;
+};
+
+export const update = async <T extends { id: string }>(label: string, input: T): Promise<T> => {
+  if (!input.id) {
+    const store = getStore();
+
+    const newInput = addDefaultCreateData<T>(input);
+    const docRef = await addDoc(collection(store, label), newInput);
+    clearCache(label);
+
+    return {
+      ...newInput,
+      id: docRef.id,
+    } as unknown as T;
+  }
+
+  return await updateById<T>(label, input);
 };
