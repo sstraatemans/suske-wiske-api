e802e30bd9c4607a6e08c2ad136f57685a0dfd60
Steven Straatemans <steven@straatemans.nl>
2022-01-20 23:50:38 +0100

    another cors test

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -2,27 +2,31 @@ import { baseHandler } from '@server/baseHandler';
 import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-});
-
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
+function initMiddleware(middleware: any) {
+  return (req: any, res: any) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
     });
-  });
 }
 
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+);
+
 const handler = baseHandler().get(async (req, res) => {
-  await runMiddleware(req, res, cors);
+  await cors(req, res);
 
   res.json({
     series: `${process.env.APIURL}/v1/series`,
