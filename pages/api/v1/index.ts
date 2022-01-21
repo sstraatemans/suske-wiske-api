30b22f1858b4fd937d2604faa5a1dac9813a1758
Steven Straatemans <steven@straatemans.nl>
2022-01-21 09:11:07 +0100

    why not

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -4,14 +4,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { RequestHandler } from 'next-connect';
 import NextCors from 'nextjs-cors';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
+const allowCors = (fn: any) => async (req: any, res: any) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another option
+  // res.setHeader('Access-Control-Allow-Origin', req.header.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
 
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  return await fn(req, res);
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   return res.json({
     series: `${process.env.APIURL}/v1/series`,
     albums: `${process.env.APIURL}/v1/albums`,
@@ -21,4 +33,4 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   });
 };
 
-export default handler;
+export default allowCors(handler);
