bc22734874e2a76567e0fe2450c2b5bd53a0a8a4
Steven Straatemans <steven@straatemans.nl>
2022-01-21 07:59:04 +0100

    cors of course

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -1,33 +1,6 @@
 import { baseHandler } from '@server/baseHandler';
-import Cors from 'cors';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function initMiddleware(middleware: any) {
-  return (req: any, res: any) =>
-    new Promise((resolve, reject) => {
-      middleware(req, res, (result: any) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
-        return resolve(result);
-      });
-    });
-}
-
-// Initialize the cors middleware
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
-);
 
 const handler = baseHandler().get(async (req, res) => {
-  await cors(req, res);
-
   res.json({
     series: `${process.env.APIURL}/v1/series`,
     albums: `${process.env.APIURL}/v1/albums`,
