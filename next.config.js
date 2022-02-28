e73bf2fdd763dd14e31ae275378e9bab3b1686ae
Steven Straatemans <steven@straatemans.nl>
2022-03-01 00:19:38 +0100

    last one of today

--- next.config.js
+++ next.config.js
@@ -2,22 +2,27 @@ const removeImports = require('next-remove-imports')();
 
 module.exports = (phase, { defaultConfig }) => {
   return removeImports({
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+          ],
+        },
+      ];
+    },
     ...defaultConfig,
     images: {
       domains: ['firebasestorage.googleapis.com'],
-    },
-    headers: {
-      source: '/api/v1/:path*',
-      headers: [
-        { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' },
-        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-        {
-          key: 'Access-Control-Allow-Headers',
-          value:
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-        },
-      ],
     },
   });
 };
--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -5,14 +5,6 @@ const handler = baseHandler()
     return res.status(200).send('ok');
   })
   .get(async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
     res.json({
       series: `${process.env.APIURL}/v1/series`,
       albums: `${process.env.APIURL}/v1/albums`,
