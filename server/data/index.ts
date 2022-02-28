b9db3c637fb4322c09ec628825c62b2ed35d615c
Steven Straatemans <steven@straatemans.nl>
2022-03-01 00:52:48 +0100

    because..why not

--- server/data/index.ts
+++ server/data/index.ts
@@ -1,15 +1,21 @@
 // Initialize Cloud Firestore through Firebase
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import * as firebaseAdmin from 'firebase-admin';
 
-export const app = initializeApp(
-  {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  },
-  '1'
-);
+let initApp;
+
+setTimeout(() => {
+  initApp = initializeApp(
+    {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    },
+    'DEFAULT'
+  );
+}, 1000);
+
+export const app = initApp;
 
 firebaseAdmin.initializeApp({
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DBURL,
