6842970447abf1aaaa8913f6bce03959b5232894
Steven Straatemans <steven@straatemans.nl>
2022-03-01 01:01:01 +0100

    last try of the day..race conditions

--- server/data/index.ts
+++ server/data/index.ts
@@ -1,24 +1,25 @@
 // Initialize Cloud Firestore through Firebase
-import { initializeApp, getApps } from 'firebase/app';
+import * as firebase from 'firebase/app';
 import * as firebaseAdmin from 'firebase-admin';
 
-let initApp;
+let firebaseAppDefined = false;
 
-setTimeout(() => {
-  initApp = initializeApp(
-    {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-    },
-    'DEFAULT'
-  );
-}, 1000);
-
-export const app = initApp;
-
-firebaseAdmin.initializeApp({
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DBURL,
+export const app = firebase.initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
 });
+
+setInterval(() => {
+  if (!firebaseAppDefined) {
+    if (firebase.getApp()) {
+      firebaseAdmin.initializeApp({
+        databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DBURL,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+      });
+
+      firebaseAppDefined = true;
+    }
+  }
+}, 100);
